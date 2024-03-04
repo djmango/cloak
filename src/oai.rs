d9439e6975011e1aa397004a8d4e9eca8e8dc425
Sulaiman Ghori <sulaiman.ghori@outlook.com>
2024-03-03 19:33:54 -0800

    :fire: (Prune) Remove dead code

--- src/oai.rs
+++ src/oai.rs
@@ -1,77 +1,12 @@
-use actix_web::{get, post, web, Error, HttpResponse, Responder};
+use actix_web::{post, web, Error, HttpResponse, Responder};
 use async_openai::error::OpenAIError;
-use async_openai::types::{
-    ChatCompletionRequestAssistantMessageArgs, ChatCompletionRequestSystemMessageArgs,
-    ChatCompletionRequestUserMessageArgs, CreateChatCompletionRequest,
-    CreateChatCompletionRequestArgs, CreateChatCompletionStreamResponse,
-};
+use async_openai::types::{CreateChatCompletionRequest, CreateChatCompletionStreamResponse};
 use bytes::Bytes;
 use futures::stream::StreamExt;
 use serde_json::to_string;
-use tracing::info; // For serializing your data to a JSON String
+use tracing::info;
 
 use crate::AppState;
-
-#[get("/ai")]
-async fn ai(state: web::Data<AppState>) -> Result<impl Responder, actix_web::Error> {
-    info!("AI endpoint hit");
-
-    let request = CreateChatCompletionRequestArgs::default()
-        .max_tokens(512u16)
-        .model("gpt-3.5-turbo")
-        .messages([
-            ChatCompletionRequestSystemMessageArgs::default()
-                .content("You are a helpful assistant.")
-                .build()
-                .map_err(|e| actix_web::error::ErrorInternalServerError(e.to_string()))?
-                .into(),
-            ChatCompletionRequestUserMessageArgs::default()
-                .content("Who won the world series in 2020?")
-                .build()
-                .map_err(|e| actix_web::error::ErrorInternalServerError(e.to_string()))?
-                .into(),
-            ChatCompletionRequestAssistantMessageArgs::default()
-                .content("The Los Angeles Dodgers won the World Series in 2020.")
-                .build()
-                .map_err(|e| actix_web::error::ErrorInternalServerError(e.to_string()))?
-                .into(),
-            ChatCompletionRequestUserMessageArgs::default()
-                .content("Where was it played? And the next one")
-                .build()
-                .map_err(|e| actix_web::error::ErrorInternalServerError(e.to_string()))?
-                .into(),
-        ])
-        .build()
-        .map_err(|e| actix_web::error::ErrorInternalServerError(e.to_string()))?;
-
-    // Call API
-    let response = state
-        .oai_client
-        .chat()
-        .create(request)
-        .await
-        .map_err(|e| actix_web::error::ErrorInternalServerError(e.to_string()))?;
-
-    let content = response
-        .choices
-        .first()
-        .ok_or_else(|| {
-            actix_web::error::ErrorInternalServerError("No response from OpenAI".to_string())
-        })?
-        .message
-        .content
-        .as_ref()
-        .ok_or_else(|| {
-            actix_web::error::ErrorInternalServerError(
-                "No content in response from OpenAI".to_string(),
-            )
-        })?
-        .clone();
-
-    info!("{}", &content);
-
-    Ok(HttpResponse::Ok().content_type("text/plain").body(content))
-}
 
 #[post("/v1/chat/completions")]
 async fn chat(
