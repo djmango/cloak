f1b198f3bc0314d64f064f44fec30c18b2dce818
Sulaiman Ghori <sulaiman.ghori@outlook.com>
2024-03-04 01:17:11 -0800

    :fire: (Logging) Prune a bit of logging stuff

--- src/oai.rs
+++ src/oai.rs
@@ -4,7 +4,6 @@ use async_openai::types::{CreateChatCompletionRequest, CreateChatCompletionStrea
 use bytes::Bytes;
 use futures::stream::StreamExt;
 use serde_json::to_string;
-use std::time::Instant;
 use tracing::info;
 
 use crate::AppState;
@@ -50,25 +49,20 @@ async fn chat(
 
     let stream: futures::stream::BoxStream<Result<Bytes, Error>> = response
         .map(
-            |item_result: Result<CreateChatCompletionStreamResponse, OpenAIError>| {
-                let start_time = Instant::now();
+            |item_result: Result<CreateChatCompletionStreamResponse, OpenAIError>| match item_result
+            {
+                Ok(item) => to_string(&item)
+                    .map_err(actix_web::error::ErrorInternalServerError)
+                    .map(|json_string| {
+                        let loggable_value = format!("data: {}\n\n", json_string);
+                        info!(
+                            "Transmitting: {}",
+                            loggable_value.strip_suffix("\n\n").unwrap()
+                        );
 
-                match item_result {
-                    Ok(item) => {
-                        to_string(&item)
-                            .map_err(actix_web::error::ErrorInternalServerError)
-                            .map(|json_string| {
-                                let elapsed_ms = start_time.elapsed().as_millis();
-                                println!("Item processed in {}ms", elapsed_ms); // Log processing time
-
-                                let loggable_value = format!("data: {}\n\n", json_string);
-                                println!("Transmitting: {}", loggable_value); // Log final value being transmitted
-
-                                Bytes::from(loggable_value)
-                            })
-                    }
-                    Err(e) => Err(actix_web::error::ErrorInternalServerError(e.to_string())),
-                }
+                        Bytes::from(loggable_value)
+                    }),
+                Err(e) => Err(actix_web::error::ErrorInternalServerError(e.to_string())),
             },
         )
         .boxed();
