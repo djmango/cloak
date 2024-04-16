457710d130a2b2c7889cf0a33a1237662db6e853
Sulaiman Ghori <sulaiman.ghori@outlook.com>
2024-04-16 18:02:56 -0400

    :recycle: (LLM) Swap all LLM backends to KeywordsAI, much simpler
    let them do the hard work and integrate litellm lol

--- src/config.rs
+++ src/config.rs
@@ -3,18 +3,10 @@ use shuttle_runtime::SecretStore;
 
 #[derive(Clone)]
 pub struct AppConfig {
-    pub openai_api_key: String,
-    pub openrouter_api_key: String,
-    pub groq_api_key: String,
-    pub perplexity_api_key: String,
-    pub together_api_key: String,
-    pub helicone_api_key: String,
+    pub keywords_api_key: String,
     pub workos_api_key: String,
     pub workos_client_id: String,
     pub jwt_secret: String,
-    pub aws_region: String,
-    pub aws_access_key_id: String,
-    pub aws_secret_access_key: String,
     pub stripe_secret_key: String,
     pub sentry_dsn: String,
 }
@@ -22,29 +14,9 @@ pub struct AppConfig {
 impl AppConfig {
     // Asynchronous factory function for creating AppConfig
     pub fn new(secret_store: &SecretStore) -> Result<Self, anyhow::Error> {
-        let openai_api_key = secret_store
-            .get("OPENAI_API_KEY")
-            .ok_or_else(|| anyhow!("OPENAI_API_KEY not found"))?;
-
-        let openrouter_api_key = secret_store
-            .get("OPENROUTER_API_KEY")
-            .ok_or_else(|| anyhow!("OPENROUTER_API_KEY not found"))?;
-
-        let groq_api_key = secret_store
-            .get("GROQ_API_KEY")
-            .ok_or_else(|| anyhow!("GROQ_API_KEY not found"))?;
-
-        let perplexity_api_key = secret_store
-            .get("PERPLEXITY_API_KEY")
-            .ok_or_else(|| anyhow!("PERPLEXITY_API_KEY not found"))?;
-
-        let together_api_key = secret_store
-            .get("TOGETHER_API_KEY")
-            .ok_or_else(|| anyhow!("TOGETHER_API_KEY not found"))?;
-
-        let helicone_api_key = secret_store
-            .get("HELICONE_API_KEY")
-            .ok_or_else(|| anyhow!("HELICONE_API_KEY not found"))?;
+        let keywords_api_key = secret_store
+            .get("KEYWORDS_API_KEY")
+            .ok_or_else(|| anyhow!("KEYWORDS_API_KEY not found"))?;
 
         let workos_api_key = secret_store
             .get("WORKOS_API_KEY")
@@ -58,18 +30,6 @@ impl AppConfig {
             .get("JWT_SECRET")
             .ok_or_else(|| anyhow!("JWT_SECRET not found"))?;
 
-        let aws_region = secret_store
-            .get("AWS_REGION")
-            .ok_or_else(|| anyhow!("AWS_REGION not found"))?;
-
-        let aws_access_key_id = secret_store
-            .get("AWS_ACCESS_KEY_ID")
-            .ok_or_else(|| anyhow!("AWS_ACCESS_KEY_ID not found"))?;
-
-        let aws_secret_access_key = secret_store
-            .get("AWS_SECRET_ACCESS_KEY")
-            .ok_or_else(|| anyhow!("AWS_SECRET_ACCESS_KEY not found"))?;
-
         let stripe_secret_key = secret_store
             .get("STRIPE_SECRET_KEY")
             .ok_or_else(|| anyhow!("STRIPE_SECRET_KEY not found"))?;
@@ -79,18 +39,10 @@ impl AppConfig {
             .ok_or_else(|| anyhow!("SENTRY_DSN not found"))?;
 
         Ok(AppConfig {
-            openai_api_key,
-            openrouter_api_key,
-            groq_api_key,
-            perplexity_api_key,
-            together_api_key,
-            helicone_api_key,
+            keywords_api_key,
             workos_api_key,
             workos_client_id,
             jwt_secret,
-            aws_region,
-            aws_access_key_id,
-            aws_secret_access_key,
             stripe_secret_key,
             sentry_dsn,
         })
