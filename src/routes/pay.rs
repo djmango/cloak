45e43ab13324429c786adf57f38501b36dda74da
Sulaiman Ghori <sulaiman.ghori@outlook.com>
2024-05-15 17:05:28 -0700

    :rocket: (Pay) Give small free trial and small bug fix with images

--- src/routes/pay.rs
+++ src/routes/pay.rs
@@ -179,7 +179,7 @@ async fn checkout(
 
     // If a user invite is found, search for the promotion code and retrieve its ID
     let subscription_data = stripe::CreateCheckoutSessionSubscriptionData {
-        // trial_period_days: Some(10),
+        trial_period_days: Some(1),
         ..Default::default()
     };
 
