79b71d21595f333e74d09f841aaf4df48a347923
Sulaiman Ghori <sulaiman.ghori@outlook.com>
2024-06-11 16:10:32 -0700

    :sparkles: (Chat) Implement autorename chat

--- src/routes/chat.rs
+++ src/routes/chat.rs
@@ -2,6 +2,13 @@ use crate::middleware::auth::AuthenticatedUser;
 use crate::models::chat::Chat;
 use crate::AppState;
 use actix_web::{delete, put, web, Error, HttpResponse};
+use async_openai::config::OpenAIConfig;
+use async_openai::types::{
+    ChatCompletionRequestAssistantMessage, ChatCompletionRequestMessage,
+    ChatCompletionRequestUserMessage, ChatCompletionRequestUserMessageContent,
+    CreateChatCompletionRequest,
+};
+use async_openai::Client;
 use serde::Deserialize;
 use std::sync::Arc;
 use tracing::error;
@@ -12,13 +19,78 @@ struct UpdateChatRequest {
     name: String,
 }
 
+#[derive(Deserialize)]
+struct AutorenameChatRequest {
+    text: String,
+}
+
+#[put("/{chat_id}/autorename")]
+async fn autorename_chat(
+    app_state: web::Data<Arc<AppState>>,
+    authenticated_user: AuthenticatedUser,
+    chat_id: web::Path<Uuid>,
+    web::Json(autorename_chat_request): web::Json<AutorenameChatRequest>,
+) -> Result<web::Json<Chat>, Error> {
+    let client: Client<OpenAIConfig> = app_state.keywords_client.clone();
+
+    let request = CreateChatCompletionRequest {
+        messages: vec![
+            ChatCompletionRequestMessage::User( ChatCompletionRequestUserMessage {
+                content: ChatCompletionRequestUserMessageContent::Text(
+                    "Create a concise, 3-5 word phrase as a header for the following. Please return only the 3-5 word header and no additional words or characters: \"yo where are pirate bases\"".to_string()
+                ),
+                ..Default::default()
+            }),
+            ChatCompletionRequestMessage::Assistant(ChatCompletionRequestAssistantMessage {
+                content: Some("Pirate Fortresses and their Origins".to_string()),
+                ..Default::default()
+            }),
+            ChatCompletionRequestMessage::User( ChatCompletionRequestUserMessage {
+                content: ChatCompletionRequestUserMessageContent::Text(format!(
+                    "Create a concise, 3-5 word phrase as a header for the following. Please return only the 3-5 word header and no additional words or characters: \"{}\"",
+                    autorename_chat_request.text
+                )),
+                ..Default::default()
+            },
+        )
+        ],
+        model: "groq/llama3-8b-8192".to_string(),
+        max_tokens: Some(64),
+        ..Default::default()
+    };
+
+    let response = client.chat().create(request).await.map_err(|e| {
+        error!("Failed to create chat: {:?}", e);
+        actix_web::error::ErrorInternalServerError(e)
+    })?;
+
+    let name = response
+        .choices
+        .first()
+        .and_then(|choice| choice.message.content.clone())
+        .unwrap_or("New Chat".to_string());
+
+    let chat = Chat::update_name(
+        &app_state.pool,
+        chat_id.into_inner(),
+        &authenticated_user.user_id,
+        &name,
+    )
+    .await
+    .map_err(|e| {
+        error!("Failed to update chat: {:?}", e);
+        actix_web::error::ErrorInternalServerError(e)
+    })?;
+    Ok(web::Json(chat))
+}
+
 #[put("/{chat_id}")]
 async fn update_chat(
     app_state: web::Data<Arc<AppState>>,
     authenticated_user: AuthenticatedUser,
     chat_id: web::Path<Uuid>,
     web::Json(update_chat_request): web::Json<UpdateChatRequest>,
-) -> Result<HttpResponse, Error> {
+) -> Result<web::Json<Chat>, Error> {
     let chat = Chat::update_name(
         &app_state.pool,
         chat_id.into_inner(),
@@ -30,7 +102,7 @@ async fn update_chat(
         error!("Failed to update chat: {:?}", e);
         actix_web::error::ErrorInternalServerError(e)
     })?;
-    Ok(HttpResponse::Ok().json(chat))
+    Ok(web::Json(chat))
 }
 
 #[delete("/{chat_id}")]
